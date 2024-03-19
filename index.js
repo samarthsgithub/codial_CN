adcc5923577cb7e1704eae184a803bf23a835a28
samarth bhardwaj <samarthbhardwaj@samarths-MacBook-Air.local>
2024-03-20 02:00:50 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 const port = 8000;
 
+// use express router
+app.use ('/',require('./routes'))
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`server is running on port ${port}`);
-
+    console.log('i am samarth the boss');
 })
