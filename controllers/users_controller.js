a0611696a26bf8bb71d02f467e799f938d20e60f
samarth bhardwaj <samarthbhardwaj@samarths-MacBook-Air.local>
2024-03-20 03:21:05 +0530

    something done here

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function(req,res){
-     res.end('<h1>User Profile</h1>');
+     return res.render('user_profile',{
+        title:"Samarth"
+     })
 }
 module.exports.activity = function(req,res){
    return res.end('<h1>Activity is being showed</h1>');
